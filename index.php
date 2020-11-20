5e6df29183b35dc4d6161096aec5ab4508b5fa12
j-zygmunt <48066671+j-zygmunt@users.noreply.github.com>
2020-11-20 23:16:02 +0100

    styled register page

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require "Routing.php";
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
+Router::get('index', 'DefaultController');
 Router::get('', 'DefaultController');
 Router::get('home', 'DefaultController');
 Router::get('register', 'DefaultController');
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -5,20 +5,17 @@ require_once 'AppController.php';
 class DefaultController extends AppController {
 
     public function index() {
-        //TODO login.html
-        //die("index method");
+
         $this->render('login');
     }
 
     public function home() {
-        //TODO home.html
-        //die("home method");
+
         $this->render('home');
     }
 
     public function register() {
-        //TODO home.html
-        //die("register method");
+
         $this->render('register');
     }
 
